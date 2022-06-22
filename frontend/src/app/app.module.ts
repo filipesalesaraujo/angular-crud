8282d247f03de38ce8e286905ec857ee1ad9b7e2
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-06-21 23:47:27 -0300

    Componente Home

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HeaderComponent } from "./components/template/header/header.component";
 import { FooterComponent } from "./components/template/footer/footer.component";
 import { NavComponent } from "./components/template/nav/nav.component";
+import { HomeComponent } from './views/home/home.component';
 
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatListModule } from "@angular/material/list";
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -22,6 +24,7 @@ import { MatListModule } from "@angular/material/list";
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent],
